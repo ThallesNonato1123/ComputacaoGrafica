6d0a8296e20684e219fe1fabdae28c61709ef0e3
Thalles Nonato <eusouthalles@gmail.com>
2022-10-31 22:00:49 -0300

    Adicionado alteração de sentido

--- Trabalho3/js/handlers/keyHandlers.js
+++ Trabalho3/js/handlers/keyHandlers.js
@@ -2,18 +2,31 @@ let keys = []
 
 const actions = {
     w: () => head.rotate('y', 0.1),
+    W: () => head.rotate('y', -0.1),
     e: () => arms.right.rotate('x', -0.1),
+    E: () => arms.right.rotate('x', 0.1),
     d: () => forearms.right.rotate('x', -0.1),
+    D: () => forearms.right.rotate('x', 0.1),
     c: () => hands.right.rotate('y', -0.1),
+    C: () => hands.right.rotate('y', 0.1),
     q: () => arms.left.rotate('x', -0.1),
+    Q: () => arms.left.rotate('x', 0.1),
     a: () => forearms.left.rotate('x', -0.1),
+    A: () => forearms.left.rotate('x', 0.1),
     z: () => hands.left.rotate('y', -0.1),
-    E: () => legs.right.rotate('x', -0.1),
-    D: () => calfs.right.rotate('x', -0.1),
-    C: () => foots.right.rotate('y', -0.1),
-    Q: () => legs.left.rotate('x', -0.1),
-    A: () => calfs.left.rotate('x', -0.1),
-    Z: () => foots.left.rotate('y', -0.1)
+    Z: () => hands.left.rotate('y', 0.1),
+    u: () => legs.left.rotate('x', -0.1),
+    U: () => legs.left.rotate('x', 0.1),
+    j: () => calfs.left.rotate('x', -0.1),
+    J: () => calfs.left.rotate('x', 0.1),
+    n: () => foots.left.rotate('y', -0.1),
+    N: () => foots.left.rotate('y', 0.1),
+    o: () => legs.right.rotate('x', -0.1),
+    O: () => legs.right.rotate('x', 0.1),
+    l: () => calfs.right.rotate('x', -0.1),
+    L: () => calfs.right.rotate('x', 0.1),
+    m: () => foots.right.rotate('y', -0.1),
+    M: () => foots.right.rotate('y', 0.1),
 }
 
 const onKeyDown = ({ key }) => {
